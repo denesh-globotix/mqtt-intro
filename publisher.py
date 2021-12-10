58d80a7952121b4c1fecfa4f2b7d174245642e4e
globotix <denesh.siva@globotix.sg>
2021-12-10 22:49:41 +0800

    Publishing to a public broker

--- publisher.py
+++ publisher.py
@@ -5,9 +5,11 @@ with open("config.yaml", "r") as f:
     try:
         config_params = (yaml.safe_load(f))
         topic = config_params['topic']
+        host = config_params['host']
+        message = config_params['message']
     except yaml.YAMLError as e:
         print(e)
 
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
-client.publish(topic, payload="on")
+client.connect(host, 1883, 60)
+client.publish(topic, payload=message, retain=True)
--- subscriber.py
+++ subscriber.py
@@ -1,30 +1,35 @@
-import paho.mqtt.client as mqtt 
+import paho.mqtt.client as mqtt
 import yaml
 
-# The callback for when the client receives a response from the server 
+# The callback for when the client receives a response from the server
+
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with client code {client}")
     print(f"Connected with User Data {userdata}")
     print(f"Connected with flags {flags}")
     print(f"Connected with result code {str(rc)}")
 
+
 def on_message(client, userdata, msg):
     print(f"{msg.topic} {str(msg.payload)}")
 
-if __name__ ==  '__main__':
+
+if __name__ == '__main__':
     client = mqtt.Client()
-    client.on_connect = on_connect 
+    client.on_connect = on_connect
     client.on_message = on_message
-    
-    client.connect("localhost", port=1883, keepalive=60)
+
 
     with open("config.yaml", "r") as f:
         try:
             config_params = (yaml.safe_load(f))
             topic = config_params['topic']
+            host = config_params['host']
         except yaml.YAMLError as e:
             print(e)
 
+    client.connect(host, port=1883, keepalive=60)
     client.subscribe(topic)
 
     client.loop_forever()
